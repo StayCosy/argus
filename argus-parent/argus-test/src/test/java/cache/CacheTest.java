71c122a7f7936d795f067ee80b3976a6f586597e
Cosy <staycosy@163.com>
2021-05-12 19:39:30 +0800

    add jetcache test example

---
+++ argus-parent/argus-test/src/test/java/cache/CacheTest.java
@@ -0,0 +1,18 @@
+package cache;
+
+import org.junit.Test;
+
+/**
+ * TODO
+ *
+ * @author COSY
+ * @version 1.0
+ * @date 2021/5/12 19:28
+ */
+public class CacheTest{
+    @Test
+    public void testJetCache(){
+        System.out.println("test jetcache!");
+    }
+
+}
