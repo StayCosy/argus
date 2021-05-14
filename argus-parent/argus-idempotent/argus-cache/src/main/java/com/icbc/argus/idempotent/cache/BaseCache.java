a6a63fee1c4ae49fd59c728f50ccce7f1177a61a
Cosy <staycosy@163.com>
2021-05-14 13:54:09 +0800

    add function

---
+++ argus-parent/argus-idempotent/argus-cache/src/main/java/com/icbc/argus/idempotent/cache/BaseCache.java
@@ -0,0 +1,15 @@
+package com.icbc.argus.idempotent.cache;
+
+import java.util.function.Function;
+
+/**
+ * TODO
+ *
+ * @author COSY
+ * @version 1.0
+ * @date 2021/5/14 10:54
+ */
+@FunctionalInterface
+public interface BaseCache extends Function {
+
+}
---
+++ argus-parent/argus-idempotent/argus-cache/src/main/java/com/icbc/argus/idempotent/cache/redis/RedisExcuter.java
@@ -0,0 +1,13 @@
+package com.icbc.argus.idempotent.cache.redis;
+
+import com.icbc.argus.idempotent.cache.BaseCache;
+
+/**
+ * TODO
+ *
+ * @author COSY
+ * @version 1.0
+ * @date 2021/5/14 10:56
+ */
+public class RedisExcuter implements BaseCache {
+}
