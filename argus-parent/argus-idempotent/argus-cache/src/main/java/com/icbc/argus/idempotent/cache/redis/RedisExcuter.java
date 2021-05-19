a7decdafe779abefb1d60dc7291ed56532277e2a
Cosy <staycosy@163.com>
2021-05-19 22:16:49 +0800

    add impl

--- argus-parent/argus-idempotent/argus-cache/src/main/java/com/icbc/argus/idempotent/cache/redis/RedisExcuter.java
+++ argus-parent/argus-idempotent/argus-cache/src/main/java/com/icbc/argus/idempotent/cache/redis/RedisExcuter.java
@@ -2,6 +2,8 @@ package com.icbc.argus.idempotent.cache.redis;
 
 import com.icbc.argus.idempotent.cache.BaseCache;
 
+import java.util.function.Function;
+
 /**
  * TODO
  *
@@ -10,4 +12,18 @@ import com.icbc.argus.idempotent.cache.BaseCache;
  * @date 2021/5/14 10:56
  */
 public class RedisExcuter implements BaseCache {
+    @Override
+    public Object apply(Object o) {
+        return null;
+    }
+
+    @Override
+    public Function andThen(Function function) {
+        return null;
+    }
+
+    @Override
+    public Function compose(Function function) {
+        return null;
+    }
 }
